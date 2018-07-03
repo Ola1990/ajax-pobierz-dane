0021b26beb0e62474fed1a003ca34b766781c655
Ola Goźlińska <aleksandra.gozlinska@wp.pl>
2018-07-03 12:53:58 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 
 $(function(){
     'use strict';
-    
-    let btn = $("button");
+    var btn = $("button");
     
     btn.on("click", function(){
       
         
         $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function(data){
             $("#dane-programisty").html(data.imie + " " + data.nazwisko + " " + data.zawod + " " + data.firma);
-        })
+        });
     });
 });
