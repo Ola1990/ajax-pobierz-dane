053edbdcc53aee4bfe64ac55f857e042258bf605
Ola Goźlińska <aleksandra.gozlinska@wp.pl>
2018-07-04 22:00:24 +0200

    poprawka

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-
-$(function(){
+$(function () {
     'use strict';
     var btn = $("button");
-    
-    btn.on("click", function(){
-      
-        
-        $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function(data){
+    $("button").add("<div id='dane-programisty'></div>").appendTo(document.body);
+
+    btn.on("click", function () {
+
+        $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function (data) {
+
             $("#dane-programisty").html(data.imie + " " + data.nazwisko + " " + data.zawod + " " + data.firma);
+
         });
     });
 });
